181a13d96d632706be9dc690dc215ddd7c79cf78
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-07-24 23:06:23 +0200

    Add js function to output computerSelection Add js function to playRound

---
+++ scripts.js
@@ -0,0 +1,34 @@
+function getComputerChoice() {
+  let calcProbability = Math.random() * 100;
+  console.log("calcProb is " + calcProbability);
+  if (calcProbability < 33.33) {
+    return "Rock";
+  } else if (calcProbability < 66.66) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection === playerSelection) {
+    return "Draw! Play again!";
+  } else if (
+    (computerSelection === "Rock" && playerSelection === "Paper") ||
+    (computerSelection === "Scissors" && playerSelection === "Rock") ||
+    (computerSelection === "Paper" && playerSelection === "Scissors")
+  ) {
+    return "Player wins!";
+  } else if (
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")
+  ) {
+    return "Player wins!";
+  } else {
+    return "error scenario";
+  }
+
+  // Include case insensitive
+  // Include player input?
+}
