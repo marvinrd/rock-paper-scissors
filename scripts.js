e701f69a6dde335e227815c7b39cf640e6e2bf15
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-07-25 14:33:52 +0200

    Initialize variables Remove debug console.log() code Add dynamic result message Add capitalization of player input

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
+//Function to generate computer choice
 function getComputerChoice() {
+  // with 1/3 probability to generate Rock, Paper or Scissors
   let calcProbability = Math.random() * 100;
-  console.log("calcProb is " + calcProbability);
   if (calcProbability < 33.33) {
     return "Rock";
   } else if (calcProbability < 66.66) {
@@ -10,31 +11,39 @@ function getComputerChoice() {
   }
 }
 
-let playerSelection = "Rock";
+//Initialize default values
+let playerInput = "sciSSorS";
+let computerSelection = "xyz";
 
+//Capitalize player input
+let playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
+
+//Function to play a round of RPS
 function playRound(playerSelection, computerSelection) {
-    
-
+  //Calculate computer choice for current round
+  computerSelection = getComputerChoice();
+  //Return result for Draw scenario
   if (computerSelection === playerSelection) {
     return "Draw! Play again!";
-  } else if (
+  } 
+  //Return result for Win scenario
+  else if (
     (computerSelection === "Rock" && playerSelection === "Paper") ||
     (computerSelection === "Scissors" && playerSelection === "Rock") ||
     (computerSelection === "Paper" && playerSelection === "Scissors")
   ) {
-    return "Player wins!";
-  } else if (
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } 
+  //Return result for Lose scenario
+  else if (
     (computerSelection === "Paper" && playerSelection === "Rock") ||
     (computerSelection === "Rock" && playerSelection === "Scissors") ||
     (computerSelection === "Scissors" && playerSelection === "Paper")
   ) {
-    return "Player wins!";
-  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } 
+  //Return result for error scenario
+  else {
     return "error scenario";
-  };
-
-  
-
-  // Include case insensitive
-  // Include player input?
-}
+  }
+};
