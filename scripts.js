c3027154cfcc1e476a616c76bc9565905ccb17c8
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-08-02 21:16:47 +0200

    Add Reset Game Button Reorder code to have html refs on top followed by initialized values Remove legacy code for running 5 game rounds through console Centered formatting for all elements

--- scripts.js
+++ scripts.js
@@ -1,6 +1,48 @@
+// references to html elements
+const resultsContainer = document.querySelector(".resultContainer");
+const totalContainer = document.querySelector(".total");
+const playOnce = document.querySelectorAll(".playSelect");
+const resetGame = document.querySelector("#resetGame");
+
+//Initialize default values
+let playerSelection = "sciSSorS";
+let computerSelection = "xyz";
+let gameRounds = 0;
+let playerScore = 0;
+let computerScore = 0;
+let maxRounds = 5;
+
+//reset game listener
+resetGame.addEventListener("click", function () {
+  gameRounds = 0;
+  playerScore = 0;
+  computerScore = 0;
+
+  const toRemove = document.querySelectorAll(".points");
+  toRemove.forEach(function (node) {
+    node.remove();
+
+    totalContainer.textContent = `Click to Rock, Paper or Scissors to start the game`;
+  });
+});
+
+//RPS listener
+playOnce.forEach(function (play) {
+  play.addEventListener("click", function () {
+    playRound(play.id);
+  });
+});
+
+//function to create new result row
+const appendToParent = function (string, parent) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "points");
+  parent.appendChild(div);
+  div.textContent = string;
+};
+
 //Function to generate computer choice
 function getComputerChoice() {
-  // with 1/3 probability to generate Rock, Paper or Scissors
   let calcProbability = Math.random() * 100;
   if (calcProbability < 33.33) {
     return "Rock";
@@ -11,106 +53,74 @@ function getComputerChoice() {
   }
 }
 
-//Initialize default values
-let playerSelection = "sciSSorS";
-let computerSelection = "xyz";
-
 //Function to play a round of RPS
 function playRound(playerSelection, computerSelection) {
   //Capitalize player input
-  playerSelection =
-    playerSelection.charAt(0).toUpperCase() +
-    playerSelection.slice(1).toLowerCase();
-  //Calculate computer choice for current round
-  computerSelection = getComputerChoice();
-  //Return result for Draw scenario
-  if (computerSelection === playerSelection) {
-    appendToParent(`Draw! Both ${playerSelection} vs ${computerSelection}`, resultsContainer);
-    return "Draw! Play again!";
-  }
-  //Return result for Win scenario
-  else if (
-    (computerSelection === "Rock" && playerSelection === "Paper") ||
-    (computerSelection === "Scissors" && playerSelection === "Rock") ||
-    (computerSelection === "Paper" && playerSelection === "Scissors")
-  ) {
-    appendToParent(`You Win! ${playerSelection} beats ${computerSelection}`,resultsContainer);
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  }
-  //Return result for Lose scenario
-  else if (
-    (computerSelection === "Paper" && playerSelection === "Rock") ||
-    (computerSelection === "Rock" && playerSelection === "Scissors") ||
-    (computerSelection === "Scissors" && playerSelection === "Paper")
-  ) {
-    appendToParent(`You Lose! ${computerSelection} beats ${playerSelection}`, resultsContainer);
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  }
-  //Return result for error scenario
-  else {
-    return "error scenario";
-  }
-}
-
-//Create variable for number of rounds to play
-let gameRounds = 1;
-//Create variable for Player score
-let playerScore = 0;
-//Create variable for Computer score
-let computerScore = 0;
-
-////Function to play a 5 round game
-function game() {
-  //Create loop to go through 1 to number of rounds
-  for (i = 0; i < gameRounds; i++) {
-    let roundResult = playRound(prompt("Rock, Paper, Scissors?"));
-
-    //If player wins, add 1 point for player
-    if (roundResult.slice(0, 5) === `You L`) {
-      ++computerScore;
+  gameRounds++;
+  if (gameRounds == maxRounds + 1) {
+    totalContainer.textContent = `Maximum rounds of ${maxRounds} reached, please click 'Reset Game' to start another set of ${maxRounds}`;
+  } else if (gameRounds < maxRounds + 1) {
+    playerSelection =
+      playerSelection.charAt(0).toUpperCase() +
+      playerSelection.slice(1).toLowerCase();
+    //Calculate computer choice for current round
+    computerSelection = getComputerChoice();
+    //Return result for Draw scenario
+    if (computerSelection === playerSelection) {
+      appendToParent(
+        `Draw! Both ${playerSelection} vs ${computerSelection}`,
+        resultsContainer
+      );
+      totalContainer.textContent = `Round ${gameRounds} - Player: ${playerScore} vs. Computer: ${computerScore}`;
+      return "Draw! Play again!";
     }
-    //If computer wins, add 1 point for player
-    if (roundResult.slice(0, 5) === `You W`) {
-      ++playerScore;
+    //Return result for Win scenario
+    else if (
+      (computerSelection === "Rock" && playerSelection === "Paper") ||
+      (computerSelection === "Scissors" && playerSelection === "Rock") ||
+      (computerSelection === "Paper" && playerSelection === "Scissors")
+    ) {
+      appendToParent(
+        `You Win! ${playerSelection} beats ${computerSelection}`,
+        resultsContainer
+      );
+      playerScore++;
+      totalContainer.textContent = `Round ${gameRounds} - Player: ${playerScore} vs. Computer: ${computerScore}`;
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
-
-    //Log the winner of the round
-    console.log(
-      `Round${i + 1}: Player: ${playerScore} vs. Computer ${computerScore}`
-    );
+    //Return result for Lose scenario
+    else if (
+      (computerSelection === "Paper" && playerSelection === "Rock") ||
+      (computerSelection === "Rock" && playerSelection === "Scissors") ||
+      (computerSelection === "Scissors" && playerSelection === "Paper")
+    ) {
+      appendToParent(
+        `You Lose! ${computerSelection} beats ${playerSelection}`,
+        resultsContainer
+      );
+      computerScore++;
+      totalContainer.textContent = `Round ${gameRounds} - Player: ${playerScore} vs. Computer: ${computerScore}`;
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    //Return result for error scenario
+    else {
+      return "error scenario";
+    }
   }
-  //Return with total results and declare winner
   if (playerScore > computerScore) {
-    console.log(
-      `You Win! Player: ${playerScore} vs. Computer ${computerScore} in ${gameRounds} Rounds`
+    appendToParent(
+      `You Win! Player: ${playerScore} vs. Computer ${computerScore} in ${gameRounds} Rounds`,
+      resultsContainer
     );
   } else if (playerScore < computerScore) {
-    console.log(
-      `You Lose! Player: ${playerScore} vs. Computer ${computerScore} in ${gameRounds} Rounds`
+    appendToParent(
+      `You Lose! Player: ${playerScore} vs. Computer ${computerScore} in ${gameRounds} Rounds`,
+      resultsContainer
     );
   } else {
-    console.log(
-      `Game complete with no winner! Player: ${playerScore} vs. Computer ${computerScore} in ${gameRounds} Rounds`
+    appendToParent(
+      `Game complete with no winner! Player: ${playerScore} vs. Computer ${computerScore} in ${gameRounds} Rounds`,
+      resultsContainer
     );
   }
 }
-
-// Select all buttons based on class playSelect
-const playOnce = document.querySelectorAll(".playSelect");
-
-// for each node in that selection
-playOnce.forEach(function (play) {
-  // apply a click-event listener that runs a function
-  play.addEventListener("click", function () {
-    playRound(play.id);
-  });
-});
-
-// select the results resultsContainer
-const resultsContainer = document.querySelector(".results");
-
-const appendToParent = function (string, parent) {
-  const div = document.createElement("div");
-  parent.appendChild(div);
-  div.textContent = string;
-};
